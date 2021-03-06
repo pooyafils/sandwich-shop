3f97087b7d3cfae6e5a34c3c6c5a3824e57cd4bb
pooyafils <pooyafils@gmail.com>
2021-03-06 11:05:12 +0330

    adding reduce method

--- src/component/sandwich/Sandwich.js
+++ src/component/sandwich/Sandwich.js
@@ -2,8 +2,17 @@ import React from 'react'
 import classes from './TheSandwich.module.css'
 import SandwichIngredient from "./sandwichingridient/SandwichIngredient";
 const Sandwich=(props)=>{
-    const trasformedIngrident=Object.keys(props.ingredients)
-    .map(igKey=>{return[...Array(props.ingredients[igKey])].map((_,i)=>{ return <SandwichIngredient key={igKey+i} type={igKey}/>})})
+    let trasformedIngrident=Object.keys(props.ingredients)
+    .map(igKey=>{return[...Array(props.ingredients[igKey])]
+        .map((_,i)=>{
+            return <SandwichIngredient key={igKey+i} type={igKey}/>})})
+        .reduce((arr,el)=>{
+            return arr.concat(el)
+        },[])
+    if(trasformedIngrident.length===0){
+        trasformedIngrident=<p>please start adding ingrident</p>
+    }
+    console.log(trasformedIngrident)
  return(
      <div className={classes.Burger}>
          <SandwichIngredient type="bread-top"/>
--- src/containers/sandwich/SandwichBulider.js
+++ src/containers/sandwich/SandwichBulider.js
@@ -8,10 +8,10 @@ class sandwichBuilder extends Component{
     }*/
     state={
         ingredient:{
-            salad:1,
-            bacon:1,
-            cheese:2,
-            meat:2
+           salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
         }
     }
 render() {
